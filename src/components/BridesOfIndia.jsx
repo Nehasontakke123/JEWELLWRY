7bd481b2e2c7288fe8a239e100f69db4986bcb65
Nehasontakke123 <nehasontakke1880@gmail.com>
2025-05-21 00:02:21 +0530

    updated code

--- src/components/BridesOfIndia.jsx
+++ src/components/BridesOfIndia.jsx
@@ -1,10 +1,42 @@
+// import React from "react";
+// import "../assets/css/BridesOfIndia.css";
+// import newBrideImg from '../assets/images/ranveer-deepika.jpg';
+// import BridesShowcase from "./BridesShowcase";
+
+// const brides = [
+//   { id: 1,  image: newBrideImg },
+//   // Add more bride objects as needed
+// ];
+
+// const BridesOfIndia = () => {
+//   return (
+//     <div className="brides-india-container">
+//       <div className="brides-india-banner">
+//         {/* Optional: Add a banner image or heading here */}
+//       </div>
+
+//       <div className="brides-india-list">
+//         {brides.map((bride) => (
+//           <div key={bride.id} className="brides-india-card">
+//             <img src={bride.image} alt={bride.name} className="brides-india-img" />
+//             <p className="brides-india-name">{bride.name}</p>
+//           </div>
+//         ))}
+//       </div>
+//        <BridesShowcase/>
+//     </div>
+//   );
+// };
+
+// export default BridesOfIndia;
+
 import React from "react";
 import "../assets/css/BridesOfIndia.css";
-import newBrideImg from '../assets/images/ranveer-deepika.jpg';
+import newBrideImg from '../assets/images/Deepika_Ranveer_mehandi.jpg';
 import BridesShowcase from "./BridesShowcase";
 
 const brides = [
-  { id: 1,  image: newBrideImg },
+  { id: 1, name: "Ranveer & Deepika", image: newBrideImg },
   // Add more bride objects as needed
 ];
 
@@ -12,7 +44,7 @@ const BridesOfIndia = () => {
   return (
     <div className="brides-india-container">
       <div className="brides-india-banner">
-        {/* Optional: Add a banner image or heading here */}
+        {/* Optional banner */}
       </div>
 
       <div className="brides-india-list">
@@ -23,9 +55,12 @@ const BridesOfIndia = () => {
           </div>
         ))}
       </div>
-       <BridesShowcase/>
+
+      <BridesShowcase />
     </div>
   );
 };
 
 export default BridesOfIndia;
+
+
